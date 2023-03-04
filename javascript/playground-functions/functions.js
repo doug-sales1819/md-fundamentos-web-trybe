c7df270f6f1d0cccc857d1569c0607613b76377e
Douglas Sales <douglas.sales0611@gmail.com>
2023-03-04 13:25:51 -0300

    corrigindo problemas

--- javascript/playground-functions/functions.js
+++ javascript/playground-functions/functions.js
@@ -147,8 +147,6 @@ function generatePhoneNumber(array) {
   );
 }
 
-console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
-
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
   let isTriangle = false;
