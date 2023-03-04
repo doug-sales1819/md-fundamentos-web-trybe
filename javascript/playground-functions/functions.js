4fa494a5096b9cdd72117fa7583e67f2c747a1e0
Douglas Sales <douglas.sales0611@gmail.com>
2023-03-04 00:11:55 -0300

    adiciona o repositório javascript e playgroud-functions

---
+++ javascript/playground-functions/functions.js
@@ -0,0 +1,68 @@
+// Desafio 1
+function compareTrue(param1, param2) {
+    return param1 && param2 ? true : false;
+}
+
+// Desafio 2
+function calcArea(base, height) {
+  return `A área do triângulo (${base} * ${height}) / 2 = ${(base * height) / 2}`;
+}
+
+// Desafio 3
+function splitSentence(phrase) {
+  return phrase && typeof phrase === 'string' ? phrase.split(' ') : undefined;
+}
+
+console.log(splitSentence());
+// Desafio 4
+function concatName() {
+  
+}
+
+// Desafio 5
+function footballPoints() {
+  // seu código aqui
+}
+
+// Desafio 6
+function highestCount() {
+  // seu código aqui
+}
+
+// Desafio 7
+function catAndMouse() {
+  // seu código aqui
+}
+
+// Desafio 8
+function fizzBuzz() {
+  // seu código aqui
+}
+
+// Desafio 9
+function encode() {
+  // seu código aqui
+}
+function decode() {
+  // seu código aqui
+}
+
+// Desafio 10
+function techList() {
+  // seu código aqui
+}
+
+// Desafio 11
+function generatePhoneNumber() {
+  // seu código aqui
+}
+
+// Desafio 12
+function triangleCheck() {
+  // seu código aqui
+}
+
+// Desafio 13
+function hydrate() {
+  // seu código aqui
+}
