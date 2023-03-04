16e55e42849fa70ec7a26d89daf4391bab0b246c
Douglas Sales <douglas.sales0611@gmail.com>
2023-03-04 01:28:39 -0300

    resolução do requisito 8 do playground-functions

--- javascript/playground-functions/functions.js
+++ javascript/playground-functions/functions.js
@@ -5,12 +5,12 @@ function compareTrue(param1, param2) {
 
 // Desafio 2
 function calcArea(base, height) {
-  return `A área do triângulo (${base} * ${height}) / 2 = ${(base * height) / 2}`;
+  return `A área do triângulo (${base} * ${height}) / 2 = ${base * height / 2}`;
 }
 
 // Desafio 3
 function splitSentence(phrase) {
-  return phrase && typeof phrase === 'string' ? phrase.split(' ') : undefined;
+  return phrase && typeof phrase === "string" ? phrase.split(" ") : undefined;
 }
 
 // Desafio 4
@@ -20,7 +20,7 @@ function concatName(array) {
 
 // Desafio 5
 function footballPoints(wins, ties) {
-  return (wins * 3) + ties;
+  return wins * 3 + ties;
 }
 
 // Desafio 6
@@ -30,17 +30,33 @@ function highestCount(array) {
       ac += 1;
     }
     return ac;
-  }, 0)
+  }, 0);
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  const cat1Position = Math.abs(cat1 - mouse);
+  const cat2Position = Math.abs(cat2 - mouse);
+  const closer = cat1Position < cat2Position ? "cat1" : "cat2";
+  return cat1Position === cat2Position
+    ? "os gatos trombam e o rato foge"
+    : closer;
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(array) {
+  return array.map(val => {
+    if (val % 3 == 0 && val % 5 == 0) {
+      return "fizzBuzz";
+    }
+    if (val % 3 == 0) {
+      return "fizz";
+    }
+    if (val % 5 == 0) {
+      return "buzz";
+    }
+    return "bug!";
+  });
 }
 
 // Desafio 9
